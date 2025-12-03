b51cc1ec8d740dd93c48f62b5d89ebdf2b48d71e
Filip Madunicky <yelidric@gmail.com>
2025-12-03 11:27:44 +0100

    Implements anecdote voting and creation
    Integrates React Query for efficient data fetching and state management.
    
    It introduces functionality to create new anecdotes and vote for existing ones.
    Optimistic updates are used to ensure smooth UI transitions.
    The changes also handle potential errors during data fetching, displaying an error message to the user.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
+import { getAll, voteAnecdote } from './requests'
 
 const App = () => {
+  const queryClient = useQueryClient()
+
+  const mutation = useMutation({
+    mutationFn: voteAnecdote,
+    onSuccess: () => {
+      queryClient.invalidateQueries(['anecdotes'])
+    }
+  })
+
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAll,
+    retry: 1,
+    refetchOnWindowFocus: true
+  })
+
   const handleVote = (anecdote) => {
-    console.log('vote')
+    mutation.mutate(anecdote)
   }
 
-  const anecdotes = [
-    {
-      content: 'If it hurts, do it more often',
-      id: '47145',
-      votes: 0,
-    },
-  ]
+  const anecdotes = result.data
+
+  if (result.isError) return (
+    <p>anecdote service not available due to problems in server</p>
+  )
 
   return (
     <div>
@@ -21,7 +38,7 @@ const App = () => {
       <Notification />
       <AnecdoteForm />
 
-      {anecdotes.map((anecdote) => (
+      {result.isFetched && anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
---
+++ src/requests.js
@@ -0,0 +1,46 @@
+const baseUrl = 'http://localhost:3001/anecdotes'
+
+export const getAll = async () => {
+  const response = await fetch(baseUrl)
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch anecdotes')
+  }
+
+  return await response.json()
+}
+
+export const createAnecdote = async (content) => {
+  const options = {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify(content)
+  }
+
+  const response = await fetch(baseUrl, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to create new anecdote')
+  }
+
+  return await response.json()
+}
+
+export const voteAnecdote = async (anecdote) => {
+  const options = {
+    method: 'PUT',
+    headers: { 'Content-type': 'application/json' },
+    body: JSON.stringify({
+      ...anecdote,
+      votes: anecdote.votes + 1
+    })
+  }
+
+  const response = await fetch(`${baseUrl}/${anecdote.id}`, options)
+
+  if (!response.ok) {
+    throw new Error('Failed to vote anecdote')
+  }
+
+  return await response.json()
+}
